104848784a3351e68f7d6d77331fcca76a893b76
Bilal-Ahmed4 <bilalahmedaka1@gmail.com>
2026-07-20 00:58:06 +0500

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -18,41 +18,47 @@ func findPath(target string) (string, bool) {
 }
 
 func main() {
-	// TODO: Uncomment the code below to pass the first stage
-
 	reader := bufio.NewReader(os.Stdin)
 	builtins := []string{"echo", "exit", "type"}
 
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
-
 		line, err := reader.ReadString('\n')
 		if err != nil {
 			os.Exit(1)
 		}
 		command := strings.TrimSpace(line)
-		tokens := strings.Split(command, " ")
-		path, ok := findPath(tokens[1])
-
 		if command == "" {
 			continue
-		} else if command == "exit" {
-			os.Exit(0)
-		} else if strings.HasPrefix(command, "echo ") {
-			fmt.Fprintln(os.Stdout, command[5:])
-			continue
-		} else if tokens[0] == "type" && slices.Contains(builtins, tokens[1]) {
-			fmt.Fprintln(os.Stdout, tokens[1]+" is a shell builtin")
-			continue
-		} else if tokens[0] == "type" && ok {
-			fmt.Fprintln(os.Stdout, tokens[1], "is", path)
-			continue
-		} else if tokens[0] == "type" {
-			fmt.Fprintln(os.Stdout, tokens[1]+": not found")
-			continue
 		}
-		fmt.Fprintln(os.Stdout, command+": command not found")
 
+		tokens := strings.Split(command, " ")
+		cmd := tokens[0]
+		args := tokens[1:]
+
+		switch {
+		case cmd == "exit":
+			os.Exit(0)
+
+		case strings.HasPrefix(command, "echo "):
+			fmt.Fprintln(os.Stdout, command[5:])
+
+		case cmd == "type":
+			if len(args) == 0 {
+				continue
+			}
+			target := args[0]
+
+			if slices.Contains(builtins, target) {
+				fmt.Fprintln(os.Stdout, target+" is a shell builtin")
+			} else if path, ok := findPath(target); ok {
+				fmt.Fprintln(os.Stdout, target+" is "+path)
+			} else {
+				fmt.Fprintln(os.Stdout, target+": not found")
+			}
+
+		default:
+			fmt.Fprintln(os.Stdout, command+": command not found")
+		}
 	}
-
 }
