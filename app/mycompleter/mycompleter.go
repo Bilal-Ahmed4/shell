07804d283191457471037205e998187b0ffb6957
Bilal-Ahmed4 <bilalahmedaka1@gmail.com>
2026-07-21 23:40:00 +0500

    codecrafters submit [skip ci]

--- app/mycompleter/mycompleter.go
+++ app/mycompleter/mycompleter.go
@@ -55,7 +55,7 @@ func (c *MyCompleter) Do(line []rune, pos int) (newLine [][]rune, length int) {
 		if c.tabPressed && c.lastPrefix == prefix {
 			fmt.Println()
 			for _, match := range matches {
-				fmt.Fprint(os.Stdout, match, "  ")
+				fmt.Fprint(os.Stdout, match)
 			}
 			fmt.Println()
 			fmt.Fprint(os.Stdout, "$ ", c.lastPrefix, " ")
